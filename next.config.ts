1cbd7c82782b23e7ed63656f4f1c3cc707d3b534
UmarAbdullah79 <uu0339843@gmail.com>
2026-02-26 17:59:39 +0530

    package file chaged

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+
+  output: "export",
+
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
+    ],
+  },
+
+  experimental: {
+    optimizePackageImports: ["gsap"],
+  },
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
